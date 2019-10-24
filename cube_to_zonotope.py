1dd13bc5e8596f2bddccad5e83fd9edaa091bb79
Peng YU <peng.yu@shopify.com>
2019-10-24 14:02:28 +0200

    add new code;

--- cube_to_zonotope.py
+++ cube_to_zonotope.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from scipy.special import comb
+from scipy.spatial import ConvexHull, Delaunay
 import numpy as np
 from scipy.spatial import ConvexHull
 import itertools
@@ -12,3 +15,50 @@ def generator_to_zonotope(generators):
 def get_zonotopes(all_points):
     hull = ConvexHull(all_points)
     return hull.vertices
+
+
+def num_vertices(m, n):
+    return 2 * sum([comb(m-1, i) for i in range(n)])
+
+
+def unique_rows(S):
+    T = S.view(np.dtype((np.void, S.dtype.itemsize * S.shape[1])))
+    return np.unique(T).view(S.dtype).reshape(-1, S.shape[1])
+
+
+def bool_sign(a): return np.where(np.sign(a) > 0, 1.0, 0.0)
+
+
+def zonotope_vertices(generators, n_samples=50, n_batches=50):
+    m, n = generators.shape
+    num_verts = num_vertices(m, n)
+    res = None
+    for i in range(n_batches):
+        samples = np.random.normal(size=(n_samples, n))
+        S = np.sign(samples.dot(generators.T))
+        if res is None:
+            res = unique_rows(np.vstack((S, -S)))
+        else:
+            res = unique_rows(np.vstack((res, S, -S)))
+
+    S = bool_sign(res)
+
+    if num_verts > S.shape[0]:
+        print('Warning: {} of {} vertices found.'.format(
+            S.shape[0], num_verts))
+
+    return S.dot(generators)
+
+
+def deduplicate(x):
+  normal_x = x/np.linalg.norm(x, axis=1)[:, np.newaxis]
+  sx, sy = np.nonzero(np.isclose(normal_x.dot(normal_x.T), 1.0))
+  visited = set()
+  collect = defaultdict(set)
+  for i, j in zip(sx, sy):
+    if j in visited:
+      continue
+    else:
+      visited.add(j)
+    collect[i].add(j)
+  return np.vstack([np.sum(x[list(k)], axis=0) for k in collect.values()])
