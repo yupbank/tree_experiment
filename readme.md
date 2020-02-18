b79086ec527e77b24542b4c80c04ba1b369a11ac
Peng YU <peng.yu@shopify.com>
2020-02-18 13:56:33 +0100

    add badgets

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 # Tree splitting in Ratio Space 
 ---
 
+[![CircleCI](https://circleci.com/gh/yupbank/tree_to_tensorflow.svg?style=svg)](https://circleci.com/gh/yupbank/tree_to_tensorflow)
+[![codecov](https://codecov.io/gh/yupbank/tree_experiment/branch/master/graph/badge.svg)](https://codecov.io/gh/yupbank/tree_experiment)
+
 ## Classification in  ratio space
 
 - Multi-Class with nominal feature
--- tests/test_bsz.py
+++ tests/test_bsz.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.datasets import make_classification
 from sklearn import preprocessing
 from bsz.bsplitz import NumericalSplitter, NominalSplitter
-from bsz.utils import gini_improvement
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -21,9 +20,8 @@ def test_numerical_splitter_improvements(classification_data):
     improvements = numerical_splitter.improvements(orders, y_high_d)
     clf = DecisionTreeClassifier(max_depth=1).fit(x[:, col][:, np.newaxis], y)
     tree_improvement = np.dot(np.array([1, -1, -1]),
-                              clf.tree_.impurity*clf.tree_.n_node_samples/clf.tree_.n_node_samples[0])
+                              clf.tree_.impurity * clf.tree_.n_node_samples / clf.tree_.n_node_samples[0])
     np.testing.assert_almost_equal(np.max(improvements), tree_improvement)
-
 
 
 def test_numerical_splitter_splits_correctly(classification_data):
@@ -36,10 +34,10 @@ def test_numerical_splitter_splits_correctly(classification_data):
 
     np.testing.assert_equal(x[:, col] <= clf.tree_.threshold[0], x[:, col] <= numerical_splitter.threshold)
 
-    np.testing.assert_equal(x[:, col] <= clf.tree_.threshold[0], numerical_splitter.split(x[:, col] ))
+    np.testing.assert_equal(x[:, col] <= clf.tree_.threshold[0], numerical_splitter.split(x[:, col]))
 
 
-def test_nomial_splitter_splits(classification_data):
+def test_nominal_splitter_splits(classification_data):
     x, y = classification_data
     y_high_d = preprocessing.OneHotEncoder(sparse=False).fit_transform(y[:, np.newaxis])
     nominal_splitter = NominalSplitter()
@@ -49,5 +47,5 @@ def test_nomial_splitter_splits(classification_data):
     best_improvement = nominal_splitter.find_best(c_xi, y_high_d)
     numerical_splitter = NumericalSplitter()
     improvement_baseline = numerical_splitter.find_best(c_xi, y_high_d)
-    #print(best_improvement, improvement_baseline, numerical_splitter.find_best(xi, y_high_d))
-    assert  best_improvement >= improvement_baseline
+    # print(best_improvement, improvement_baseline, numerical_splitter.find_best(xi, y_high_d))
+    assert best_improvement >= improvement_baseline
