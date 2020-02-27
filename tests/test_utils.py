8dac7ba0fdb82a1be7ce0dee170d0948cce7f4e9
Peng YU <peng.yu@shopify.com>
2020-02-27 13:22:28 +0100

    refactor tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,4 @@
 import numpy as np
-from sklearn import preprocessing
-from sklearn.datasets import make_classification
 from bsz.utils import (
     fast_gini_improvements,
     fast_variance_improvements,
@@ -17,80 +15,50 @@ import pytest
 
 
 @pytest.fixture
-def regression_target_split():
-    data = np.random.randn(20)
+def regression_target_split(regression_data):
+    x, y = regression_data
     index = 4
-    return data, index
+    return y, index
 
 
 @pytest.fixture
-def classification_target_split():
-    x, data = make_classification()
+def classification_target_split(classification_data):
+    x, y, y_high_d = classification_data
     index = 4
-    return data, index
+    return y_high_d, index
 
 
-def test_fast_variance_the_same_with_variance(regression_target_split):
+@pytest.mark.parametrize(
+    "simple_method,vec_method",
+    [
+        (variance_improvement, fast_variance_improvements),
+        (skewness_improvement, fast_skewness_improvements),
+    ],
+)
+def test_regression_match(regression_target_split, simple_method, vec_method):
     data, index = regression_target_split
-    res = variance_improvement(data, index)
+    res = simple_method(data, index)
 
     d = _regression_summary_vector(data)
     p = _regression_summary_vector(data[:index])
 
-    f_res = fast_variance_improvements(np.array([p, p]), d)
-
+    f_res = vec_method(np.array([p, p]), d)
     np.testing.assert_allclose(res, f_res[0])
-
     np.testing.assert_allclose(f_res[0], f_res[1])
 
 
-def test_fast_skewness_the_same_with_skewness(regression_target_split):
-    data, index = regression_target_split
-    res = skewness_improvement(data, index)
-
-    d = _regression_summary_vector(data)
-    p = _regression_summary_vector(data[:index])
-
-    f_res = fast_skewness_improvements(np.array([p, p]), d)
-
-    np.testing.assert_allclose(res, f_res[0])
-
-    np.testing.assert_allclose(f_res[0], f_res[1])
-
-
-def test_fast_gini_the_same_with_gini(classification_target_split):
-    y, index = classification_target_split
-
-    data = preprocessing.OneHotEncoder(sparse=False, categories="auto").fit_transform(
-        y[:, np.newaxis]
-    )
-
-    res = gini_improvement(data, index)
-
+@pytest.mark.parametrize(
+    "simple_method,vec_method",
+    [
+        (gini_improvement, fast_gini_improvements),
+        (entropy_improvement, fast_entropy_improvements),
+    ],
+)
+def test_classification_math(classification_target_split, simple_method, vec_method):
+    data, index = classification_target_split
+    res = simple_method(data, index)
     d = _classification_summary_vector(data)
     p = _classification_summary_vector(data[:index])
-
-    f_res = fast_gini_improvements(np.array([p, p]), d)
-
+    f_res = vec_method(np.array([p, p]), d)
     np.testing.assert_allclose(res, f_res[0])
-
-    np.testing.assert_allclose(f_res[0], f_res[1])
-
-
-def test_fast_entropy_the_same_with_entropy(classification_target_split):
-    y, index = classification_target_split
-
-    data = preprocessing.OneHotEncoder(sparse=False, categories="auto").fit_transform(
-        y[:, np.newaxis]
-    )
-
-    res = entropy_improvement(data, index)
-
-    d = _classification_summary_vector(data)
-    p = _classification_summary_vector(data[:index])
-
-    f_res = fast_entropy_improvements(np.array([p, p]), d)
-
-    np.testing.assert_allclose(res, f_res[0])
-
     np.testing.assert_allclose(f_res[0], f_res[1])
